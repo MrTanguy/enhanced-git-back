93629d3305983096057b480d46b5dd1fef8ff187
MrTanguy <tanguy.meignier@gmail.com>
2025-04-08 00:05:03 +0200

    changement status

--- routers/portfolio.py
+++ routers/portfolio.py
@@ -1,6 +1,6 @@
 
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from repositories.PortfolioRepository import PortfolioRepository
 from schemas.schemas import PortfolioUpdateSchema
@@ -13,7 +13,7 @@ async def create_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)
     try:
         return PortfolioRepository().create(user_id=user_id)
     except:
-        raise HTTPException(status_code=500, detail="Internal server error")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
 
 @portfolio_router.get("/{portfolio_uuid}")
 async def get_portfolio_by_uuid(portfolio_uuid: str):
@@ -23,7 +23,7 @@ async def get_portfolio_by_uuid(portfolio_uuid: str):
 
         return portfolio
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
 
 @portfolio_router.patch("/{portfolio_uuid}")
 async def update_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)], portfolio_uuid: str, update_data: PortfolioUpdateSchema):
@@ -32,7 +32,7 @@ async def update_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)
     portfolio = portfolio_repository.get_by_uuid(portfolio_uuid)
 
     if portfolio.user_id != user_id:
-        raise HTTPException(status_code=403, detail="You do not have permission to modify this resource")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to modify this resource")
 
     try:
         updated_portfolio = portfolio_repository.update(portfolio_uuid, update_data)
@@ -41,7 +41,7 @@ async def update_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)
         raise    
     except Exception as e:
         print(f"Error updating portfolio: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
     
 @portfolio_router.delete("/{portfolio_uuid}")
 async def delete_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)], portfolio_uuid: str):
@@ -55,4 +55,4 @@ async def delete_portfolio(user_id: Annotated[int, Depends(Bearer().get_user_id)
         portfolio_repository.delete(uuid=portfolio_uuid)
     except Exception as e:
         print(f"Error deleting porfolio: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
