65c8d7a594bf06aa9e322861aa4f05cb1575914e
MrTanguy <tanguy.meignier@gmail.com>
2025-06-23 01:26:32 +0200

    refacto de /user/data afin de récupérer que la data souhaitée

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,8 @@
 import logging
 
-from typing import Annotated
+from typing import List, Optional, Annotated
 from dotenv import load_dotenv
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status, Query
 
 from repositories.UserRepository import UserRepository
 from services.security.bearer import Bearer
@@ -12,29 +12,47 @@ user_router = APIRouter()
 load_dotenv()
 
 @user_router.get("/data")
-async def get_user_data(user_id: Annotated[int, Depends(Bearer().get_user_id)]):
+async def get_user_data( 
+    user_id: Annotated[int, Depends(Bearer().get_user_id)],  
+    types: Optional[str] = Query(None, description="Comma-separated values: connections,portfolios")
+):
     try:
+        modes = ["connections", "portfolios"]
+        if types:
+            arguments = types.lower().split(",")
+
+            for argument in arguments:
+                if argument not in modes:
+                    raise HTTPException(status_code=400, detail=f"Unknown type: {argument}")
+        else:
+            arguments = modes
+
         user_data = UserRepository().read_by_id(user_id)
 
-        result = {"connections": [], "portfolios": []}
+        result = {}
 
-        for connection in user_data.connections:
-            service = init_website_service(website=connection.website)
-            username = service.getUserInfo(access_token=connection.access_token)['login']
+        if "connections" in arguments:
+            result["connections"] = []
+            for connection in user_data.connections:
+                service = init_website_service(website=connection.website)
+                username = service.getUserInfo(access_token=connection.access_token)['login']
 
-            connection_result = {"website": connection.website,
-                                 "id": connection.account_id,
-                                 "username": username}
-            
-            result["connections"].append(connection_result)
+                connection_result = {
+                    "website": connection.website,
+                    "id": connection.account_id,
+                    "username": username
+                }
+                result["connections"].append(connection_result)
 
-        for portfolio in user_data.portfolios:
-            portfolio_result = {
-                "uuid": portfolio.uuid,
-                "title": portfolio.title,
-                "content": portfolio.content
-            }
-            result["portfolios"].append(portfolio_result)
+        if "portfolios" in arguments:
+            result["portfolios"] = []
+            for portfolio in user_data.portfolios:
+                portfolio_result = {
+                    "uuid": portfolio.uuid,
+                    "title": portfolio.title,
+                    "content": portfolio.content
+                }
+                result["portfolios"].append(portfolio_result)
 
         return result
 
