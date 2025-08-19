c6c15f59b93962368b1196a5e4619fddfb529f8a
MrTanguy <tanguy.meignier@gmail.com>
2025-08-19 23:39:47 +0200

    newline

--- services/log/health_filter.py
+++ services/log/health_filter.py
@@ -6,4 +6,3 @@ class HealthFilter(logging.Filter):
     """Class to handle log configuration"""
     def filter(self, record: logging.LogRecord) -> bool:
         return "/health" not in record.getMessage()
-    
